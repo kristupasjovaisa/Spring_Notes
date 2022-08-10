825d58ffef55c04580ad85ab217e43ab2ae8879e
kristupasjovaisa <kristupas.jovaisa03@gmail.com>
2022-08-10 13:35:39 +0300

    Initial setup

---
+++ spring_notes/src/test/java/spring_notes/SpringNotesApplicationTests.java
@@ -0,0 +1,13 @@
+package spring_notes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringNotesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
