809ba44ea71a3652fe02f150e4a454cacfe04641
kristupasjovaisa <kristupas.jovaisa03@gmail.com>
2022-08-18 19:32:25 +0300

    Added NoteMapper

--- spring_notes/src/main/java/spring_notes/mapper/NoteMapper.java
+++ spring_notes/src/main/java/spring_notes/mapper/NoteMapper.java
@@ -1,7 +1,36 @@
 package spring_notes.mapper;
 
 import org.springframework.stereotype.Component;
+import spring_notes.dto.*;
+import spring_notes.entities.Note;
 
 @Component
 public class NoteMapper {
+
+    public Note mapFrom(AddNoteRequest dto) {
+        return Note.builder().
+                name(dto.getName()).
+                description(dto.getDescription()).
+                createdAt(dto.getCreatedAt()).
+                build();
+    }
+
+    public Note mapFrom(UpdateNoteRequest dto, Long id) {
+        return Note.builder().
+                id(id).
+                noteUUID(dto.getNoteUUID()).
+                name(dto.getName()).
+                description(dto.getDescription()).
+                createdAt(dto.getCreatedAt()).
+                build();
+    }
+
+    public NoteResponse mapFrom(Note note) {
+        return NoteResponse.builder().
+                noteUUID(note.getNoteUUID()).
+                name(note.getName()).
+                description(note.getDescription()).
+                createdAt(note.getCreatedAt()).
+                build();
+    }
 }
